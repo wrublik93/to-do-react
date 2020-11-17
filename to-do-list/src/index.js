f2b8f473b1eea14b19ea28c67ca0042ca4661b74
Ivan Vrubleuski <wrublik.93@mail.ru>
2020-11-17 03:04:56 +0300

    feat: react elements

--- to-do-list/src/index.js
+++ to-do-list/src/index.js
@@ -1,1 +1,14 @@
-console.log('hi');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const el = (
+    <div>
+        <h1>My Todo List</h1>
+        <input placeholder="search" />
+        <ul>
+            <li>Learn React</li>
+            <li>Drink Coffee</li>
+        </ul>
+    </div>
+);
+ReactDOM.render(el, document.getElementById('root'));
