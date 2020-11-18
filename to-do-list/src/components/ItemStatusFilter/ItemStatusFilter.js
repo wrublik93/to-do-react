38ee384aff4ea3bc010ce2193b032164f316c12b
Ivan Vrubleuski <wrublik.93@mail.ru>
2020-11-19 01:54:30 +0300

    fix: add class for item status filter and todo list item

--- to-do-list/src/components/ItemStatusFilter/ItemStatusFilter.js
+++ to-do-list/src/components/ItemStatusFilter/ItemStatusFilter.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './ItemStatusFilter.css';
 
-const ItemStatusFilter = () => {
+export default class ItemStatusFilter extends Component {
+  render() {
+    return (
+      <div className="btn-group">
+        <button
+          type="button"
+          className="btn btn-info"
+        >
+          All
+        </button>
+        <button
+          type="button"
+          className="btn btn-outline-secondary"
+        >
+          Active
+        </button>
+        <button
+          type="button"
+          className="btn btn-outline-secondary"
+        >
+          Done
+        </button>
+      </div>
+    );
+  }
+};
+
+/* const ItemStatusFilter = () => {
   return (
     <div className="btn-group">
       <button
@@ -27,4 +54,4 @@ const ItemStatusFilter = () => {
   );
 };
 
-export default ItemStatusFilter;
+export default ItemStatusFilter; */
--- to-do-list/src/components/TodoListItem/TodoListItem.js
+++ to-do-list/src/components/TodoListItem/TodoListItem.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './TodoListItem.css';
 
-const TodoListItem = ( { label, important = false } ) => {
+export default class TodoListItem extends Component {
+  render() {
+    const { label, important = false } = this.props;
+    const style = {
+      color: important ? 'steelblue' : 'black',
+      fontWeight: important ? 'bold' : 'normal',
+  };
+
+  return (
+    <span className="todo-list-item">
+      <span
+        className="todo-list-item-label"
+        style={ style }
+      >
+        { label }
+      </span>
+      <button
+        type="button"
+        className="btn btn-outline-success btn-sm float-right"
+      >
+        <i className="fa fa-exclamation" />
+      </button>
+      <button
+        type="button"
+        className="btn btn-outline-danger btn-sm float-right"
+      >
+        <i className="fa fa-trash-o" />
+      </button>
+    </span>
+  );
+  }
+}
+
+/* const TodoListItemFunc = ( { label, important = false } ) => {
     const style = {
         color: important ? 'steelblue' : 'black',
         fontWeight: important ? 'bold' : 'normal',
@@ -29,6 +62,6 @@ const TodoListItem = ( { label, important = false } ) => {
         </button>
       </span>
     );
-};
+}; */
 
-export default TodoListItem;
+/* export default TodoListItem; */
