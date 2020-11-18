3eb515366a2b970d9c7ad4b987f9992da9f621f8
Ivan Vrubleuski <wrublik.93@mail.ru>
2020-11-19 02:08:56 +0300

    feat: learn event on click

--- to-do-list/src/components/TodoListItem/TodoListItem.js
+++ to-do-list/src/components/TodoListItem/TodoListItem.js
@@ -2,8 +2,14 @@ import React, { Component } from 'react';
 import './TodoListItem.css';
 
 export default class TodoListItem extends Component {
+
+  onLabelClick = () => {
+    console.log('Done:', this.props.label);
+  }
+
   render() {
     const { label, important = false } = this.props;
+
     const style = {
       color: important ? 'steelblue' : 'black',
       fontWeight: important ? 'bold' : 'normal',
@@ -14,6 +20,7 @@ export default class TodoListItem extends Component {
       <span
         className="todo-list-item-label"
         style={ style }
+        onClick={ this.onLabelClick }
       >
         { label }
       </span>
@@ -32,7 +39,7 @@ export default class TodoListItem extends Component {
     </span>
   );
   }
-}
+};
 
 /* const TodoListItemFunc = ( { label, important = false } ) => {
     const style = {
