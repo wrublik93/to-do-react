d85c336f3d6ed77ca1863afeb21bf0a50118f429
Ivan Vrubleuski <wrublik.93@mail.ru>
2020-11-19 01:43:34 +0300

    fix: change index.js

--- to-do-list/src/index.js
+++ to-do-list/src/index.js
@@ -1,35 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppHeader from './components/AppHeader';
-import SearchPanel from './components/SearchPanel';
-import TodoList from './components/TodoList';
 
-const App = () => {
-    const todoData = [
-        {
-            id: 1,
-            label: 'Drink Coffee',
-            important: false,
-        },
-        {
-            id: 2,
-            label: 'Read documentation',
-            important: true,
-        },
-        {
-            id: 3,
-            label: 'Eat food',
-            important: false,
-        },
-    ];
-
-    return (
-        <div>
-            <AppHeader />
-            <SearchPanel />
-            <TodoList todos={ todoData } />
-        </div>
-    )
-};
+import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
