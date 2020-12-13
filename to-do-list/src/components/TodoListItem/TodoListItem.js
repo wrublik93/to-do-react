2dd16c1dfa64ad7f1d6234073d21026b9c1e64f6
Ivan Vrubleuski <wrublik.93@mail.ru>
2020-12-14 00:00:25 +0300

    feat: mark important and done

--- to-do-list/src/components/TodoListItem/TodoListItem.js
+++ to-do-list/src/components/TodoListItem/TodoListItem.js
@@ -15,19 +15,23 @@ export default class TodoListItem extends Component {
   };
 
   onLabelClick = () => {
-    this.setState({
-      done: true,
+    this.setState(({ done }) => {
+      return {
+        done: !done,
+      }
     })
   };
 
   onMarkImportant = () => {
-    this.setState({
-      important: true,
+    this.setState((state) => {
+      return {
+        important: !state.important,
+      }
     })
   };
 
   render() {
-    const { label } = this.props;
+    const { label, onDeleted } = this.props;
     const { done, important } = this.state;
 
     let classNames = 'todo-list-item';
@@ -58,6 +62,7 @@ export default class TodoListItem extends Component {
       <button
         type="button"
         className="btn btn-outline-danger btn-sm float-right"
+        onClick={onDeleted}
       >
         <i className="fa fa-trash-o" />
       </button>
