91484d096ccfb1f4bb0238ae5ff6193d6406944f
Ivan Vrubleuski <wrublik.93@mail.ru>
2020-12-22 00:29:13 +0300

    fix: delete state for todo list component

--- to-do-list/src/components/TodoList/TodoList.js
+++ to-do-list/src/components/TodoList/TodoList.js
@@ -2,7 +2,12 @@ import React from 'react';
 import TodoListItem from '../TodoListItem';
 import './TodoList.css';
 
-const TodoList = ({ todos, onDeleted }) => {
+const TodoList = ({
+  todos,
+  onDeleted,
+  onToggleImportant,
+  onToggleDone,
+}) => {
     const elements = todos.map((item) => {
         const { id, ...itemProps } = item;
         return (
@@ -10,6 +15,8 @@ const TodoList = ({ todos, onDeleted }) => {
                 <TodoListItem
                 { ...itemProps }
                 onDeleted = {() => onDeleted(id)}
+                onToggleImportant={() => onToggleImportant(id)}
+                onToggleDone={() => onToggleDone(id)}
               />
             </li>
         )
