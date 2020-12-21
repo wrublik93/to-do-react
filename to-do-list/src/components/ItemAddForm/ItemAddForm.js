ea1b0b211ca90e812d43b4d6d1aa79dace543923
Ivan Vrubleuski <wrublik.93@mail.ru>
2020-12-22 00:28:05 +0300

    feat: implement functions for item add form

--- to-do-list/src/components/ItemAddForm/ItemAddForm.js
+++ to-do-list/src/components/ItemAddForm/ItemAddForm.js
@@ -3,15 +3,41 @@ import React, { Component } from 'react';
 import './ItemAddForm.css';
 
 export default class ItemAddForm extends Component {
+  state = {
+    label: '',
+  };
+
+  onLabelChange = (e) => {
+    this.setState({
+      label: e.target.value
+    })
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onItemAdded(this.state.label);
+    this.setState({
+      label: '',
+    });
+  }
+
   render() {
     return (
-      <div className="item-add-form">
+      <form
+        className="item-add-form d-flex"
+        onSubmit={this.onSubmit}
+      >
+        <input
+          value={this.state.label}
+          className="form-control"
+          type="text"
+          onChange={this.onLabelChange}
+          placeholder="I need ..." />
         <button
-          className="btn btn-outline-secondary item-add-button"
-          onClick={ () => this.props.onItemAdded('Hi') }>
+          className="btn btn-outline-secondary item-add-button">
           Add Item
         </button>
-      </div>
+      </form>
     )
   }
 }
